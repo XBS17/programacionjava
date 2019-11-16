2e4668e8f2034a970e77044cc60a9b1c24576dd5
XBS17 <xuliablancos@gmail.com>
2019-11-16 21:50:03 +0100

    I have made a change

---
+++ suma.java
@@ -0,0 +1,41 @@
+class suma{
+
+public static void main(String[] args){
+ int n1=50; int n2=30; int n3=5;  int suma=0;
+
+suma=n1+n2;
+
+System.out.println("LA SUMA ES : " + suma);
+
+suma=suma+n3;
+
+System.out .println("LA SUMA ES : " + suma) ;
+
+}
+}
+
+
+/*class suma{
+
+public static void main(String[] args){
+int n1=50; int n2=30; int n3=5; int suma=0;
+
+suma=n1+n2;
+
+System.out.println("LA SUMA ES : " + suma);
+
+suma=suma+n3;
+
+System.out .println("LA SUMA ES : " + suma) ;
+
+}
+}
+
+
+
+La variable no es "static".
+
+En la parte de la declaración de (String[] args) había un espacio entre la g y el corchete, por lo que se trata de un error de escritura.
+
+Tengo que inicializar todas las variables con un valor, y n3 estaba sin inicializar.*/
+
