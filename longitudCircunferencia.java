916237cbb78887ecbdb84118fa21b8e1a1863e42
XBS17 <xuliablancos@gmail.com>
2019-11-23 20:31:17 +0100

    Dame la longitud de una circunferencia de radio 4

---
+++ longitudCircunferencia.java
@@ -0,0 +1,19 @@
+//Realiza un programa que calcule la longitud de una circunferencia de radio 3 metros.
+
+public class longitudCircunferencia {
+	
+	public static double CalculaLongitud() {
+		
+		double radio=4;
+		
+		return 2*Math.PI*radio;
+	}
+	
+	public static void main(String[] args){
+		
+		System.out.println(CalculaLongitud());
+		
+	}
+		
+		
+}
