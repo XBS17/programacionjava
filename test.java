913e05ff5de7ba5d5b8b3a889a7bbd0b74f62635
XBS17 <xuliablancos@gmail.com>
2019-11-23 20:01:00 +0100

    Ejercicio que me da letras de forma aleatoria

---
+++ test.java
@@ -0,0 +1,31 @@
+/*Realiza un método para la clase Test que genere letras de forma aleatoria. 
+ * Como ejercicio complementario investiga el funcionamiento y uso de la función Math.random().*/
+
+
+class Test {
+	
+	public static char giveRandomLetters() {
+	return (char) (Math.random()*(65-90)+90);	
+	}
+	
+	
+	public static void main(String[] args){
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());
+			System.out.println("La letra mayúscula aleatoria es:" + giveRandomLetters());			 
+		}
+}
